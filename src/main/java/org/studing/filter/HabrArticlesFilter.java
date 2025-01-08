3cf305ec8ee63666a7d06b4c03f77619ea59de53
Grigoriy Kryazhev <kryazhevG24@yandex.ru>
2025-01-08 18:16:18 +0300

    Проверка коллекций на null и размерность 0 через isNotEmpty

--- src/main/java/org/studing/filter/HabrArticlesFilter.java
+++ src/main/java/org/studing/filter/HabrArticlesFilter.java
@@ -11,6 +11,8 @@ import static java.lang.Integer.parseInt;
 import static java.util.Collections.unmodifiableMap;
 import static java.util.Comparator.comparing;
 import static java.util.stream.Collectors.*;
+import static org.apache.commons.collections4.CollectionUtils.isNotEmpty;
+import static org.apache.commons.collections4.MapUtils.isNotEmpty;
 
 @RequiredArgsConstructor
 public class HabrArticlesFilter {
@@ -21,48 +23,57 @@ public class HabrArticlesFilter {
     Map<String, List<String>> mapAuthorAndHisTitles;
 
     public Map<String, List<String>> getAuthorAndHisTitles() {
-        if (mapAuthorAndHisTitles == null) {
-            val mutableMap = articles.stream().collect(
-                groupingBy(
-                    HabrArticle::author,
-                    mapping(HabrArticle::title, toList())));
-            mapAuthorAndHisTitles = unmodifiableMap(mutableMap);
+        if (isNotEmpty(mapAuthorAndHisTitles)) {
+            return mapAuthorAndHisTitles;
         }
+
+        val mutableMap = articles.stream().collect(
+            groupingBy(
+                HabrArticle::author,
+                mapping(HabrArticle::title, toList())));
+        mapAuthorAndHisTitles = unmodifiableMap(mutableMap);
         return mapAuthorAndHisTitles;
     }
 
     public List<HabrArticle> getHabrArticlesLimitCountView(final int limitCountView) {
-        if (listArticlesLimitedCountView == null) {
-            listArticlesLimitedCountView = articles.stream()
-                .filter(article -> parseInt(article.countViews()) > limitCountView)
-                .sorted(comparing(HabrArticle::title))
-                .toList();
+        if (isNotEmpty(listArticlesLimitedCountView)) {
+            return listArticlesLimitedCountView;
         }
+
+        listArticlesLimitedCountView = articles.stream()
+            .filter(article -> parseInt(article.countViews()) > limitCountView)
+            .sorted(comparing(HabrArticle::title))
+            .toList();
         return listArticlesLimitedCountView;
     }
 
     public List<String> getUniqueCategories() {
-        if (listCategories == null) {
-            listCategories = articles.stream()
-                .flatMap(article -> article.categories().stream())
-                .distinct()
-                .toList();
+        if (isNotEmpty(listCategories)) {
+            return listCategories;
         }
+
+        listCategories = articles.stream()
+            .flatMap(article -> article.categories().stream())
+            .distinct()
+            .toList();
         return listCategories;
     }
 
     public List<HabrArticle> getHabrArticlesWhereTimeToReadLessThanAverage() {
-        if (listArticlesTimeToReadLessThanAverage == null) {
-            val timeToRead = articles.stream()
-                .map(article -> parseInt(article.timeToRead()))
-                .toList();
-
-            val averageTime = timeToRead.stream().mapToInt(Integer::intValue).sum() / timeToRead.size();
-
-            listArticlesTimeToReadLessThanAverage = articles.stream().
-                filter(article -> parseInt(article.timeToRead()) < averageTime).
-                toList();
+        if (isNotEmpty(listArticlesTimeToReadLessThanAverage)) {
+            return listArticlesTimeToReadLessThanAverage;
         }
+
+        val timeToRead = articles.stream()
+            .map(article -> parseInt(article.timeToRead()))
+            .toList();
+
+        val averageTime = timeToRead.stream().mapToInt(Integer::intValue).sum() / timeToRead.size();
+
+        listArticlesTimeToReadLessThanAverage = articles.stream().
+            filter(article -> parseInt(article.timeToRead()) < averageTime).
+            toList();
+
         return listArticlesTimeToReadLessThanAverage;
     }
 }
--- src/main/java/org/studing/filter/PerformanceFilter.java
+++ src/main/java/org/studing/filter/PerformanceFilter.java
@@ -9,6 +9,7 @@ import java.time.LocalDateTime;
 import java.time.LocalTime;
 import java.util.List;
 import java.util.Map;
+import java.util.Map.Entry;
 
 import static java.lang.Integer.parseInt;
 import static java.time.DayOfWeek.*;
@@ -16,6 +17,8 @@ import static java.time.LocalTime.parse;
 import static java.time.format.DateTimeFormatter.ofPattern;
 import static java.util.Comparator.comparing;
 import static java.util.stream.Collectors.*;
+import static org.apache.commons.collections4.CollectionUtils.isNotEmpty;
+import static org.apache.commons.collections4.MapUtils.isNotEmpty;
 import static org.studing.util.EnvParser.get;
 
 @RequiredArgsConstructor
@@ -32,53 +35,61 @@ public class PerformanceFilter {
     }
 
     public List<Performance> getLimitAgePerformance(final int ageLimit) {
-        if (listLimitedAgePerformance == null) {
-            val temp = performanceList.stream()
-                .filter(performance -> parseInt(performance.ageLimit()) <= ageLimit)
-                .sorted(comparing(Performance::duration))
-                .toList();
-            listLimitedAgePerformance = getListPerformanceUniqueTitle(temp);
+        if (isNotEmpty(listLimitedAgePerformance)) {
+            return listLimitedAgePerformance;
         }
+
+        val temp = performanceList.stream()
+            .filter(performance -> parseInt(performance.ageLimit()) <= ageLimit)
+            .sorted(comparing(Performance::duration))
+            .toList();
+        listLimitedAgePerformance = getListPerformanceUniqueTitle(temp);
         return listLimitedAgePerformance;
     }
 
     public List<Performance> getListPerformanceUniqueTitle() {
-        if (listPerformanceUniqueTitle == null) {
-            listPerformanceUniqueTitle = getListPerformanceUniqueTitle(performanceList);
+        if (isNotEmpty(listPerformanceUniqueTitle)) {
+            return listPerformanceUniqueTitle;
         }
+
+        listPerformanceUniqueTitle = getListPerformanceUniqueTitle(performanceList);
         return listPerformanceUniqueTitle;
     }
 
     public Map<Performance, List<LocalDateTime>> getMapTitleListDate() {
-        if (mapPerformanceListDateTime == null) {
-            val performancesUniqueTitle = getListPerformanceUniqueTitle(performanceList);
-
-            val uniqueTitleMap = performancesUniqueTitle.stream()
-                .collect(toMap(Performance::title, performance -> performance));
-
-            val mapPerformanceListDate = performanceList.stream()
-                .collect(groupingBy(
-                    performance -> uniqueTitleMap.get(performance.title()),
-                    mapping(Performance::date, toList())));
-
-            mapPerformanceListDateTime = mapPerformanceListDate.entrySet().stream()
-                .filter(entry -> !entry.getValue().isEmpty())
-                .collect(toUnmodifiableMap(Map.Entry::getKey, Map.Entry::getValue));
+        if (isNotEmpty(mapPerformanceListDateTime)) {
+            return mapPerformanceListDateTime;
         }
+
+        val performancesUniqueTitle = getListPerformanceUniqueTitle(performanceList);
+
+        val uniqueTitleMap = performancesUniqueTitle.stream()
+            .collect(toMap(Performance::title, performance -> performance));
+
+        val mapPerformanceListDate = performanceList.stream()
+            .collect(groupingBy(
+                performance -> uniqueTitleMap.get(performance.title()),
+                mapping(Performance::date, toList())));
+
+        mapPerformanceListDateTime = mapPerformanceListDate.entrySet().stream()
+            .filter(entry -> !entry.getValue().isEmpty())
+            .collect(toUnmodifiableMap(Entry::getKey, Entry::getValue));
         return mapPerformanceListDateTime;
     }
 
     public List<Performance> getPerformanceListTask4(@NonNull final LocalTime durationLimit) {
-        if (listPerformanceTask4 == null) {
-            listPerformanceTask4 = performanceList.stream()
-                .filter(performance -> performance.duration().isBefore(durationLimit))
-                .filter(performance -> performance.date().getHour() >= TIME_EVENING.getHour())
-                .filter(performance -> {
-                    val dayOfWeek = performance.date().getDayOfWeek();
-                    return dayOfWeek == SATURDAY || dayOfWeek == TUESDAY || dayOfWeek == THURSDAY;
-                })
-                .toList();
+        if (isNotEmpty(listPerformanceTask4)) {
+            return listPerformanceTask4;
         }
+
+        listPerformanceTask4 = performanceList.stream()
+            .filter(performance -> performance.duration().isBefore(durationLimit))
+            .filter(performance -> performance.date().getHour() >= TIME_EVENING.getHour())
+            .filter(performance -> {
+                val dayOfWeek = performance.date().getDayOfWeek();
+                return dayOfWeek == SATURDAY || dayOfWeek == TUESDAY || dayOfWeek == THURSDAY;
+            })
+            .toList();
         return listPerformanceTask4;
     }
 
