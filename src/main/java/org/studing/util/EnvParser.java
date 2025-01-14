7b43c622134ebf856cd075e6415ae702946de503
Grigoriy Kryazhev <kryazhevG24@yandex.ru>
2025-01-14 15:55:11 +0300

    @UtilityClass

--- src/main/java/org/studing/util/EnvParser.java
+++ src/main/java/org/studing/util/EnvParser.java
@@ -2,13 +2,15 @@ package org.studing.util;
 
 import io.github.cdimascio.dotenv.Dotenv;
 import lombok.NonNull;
+import lombok.experimental.UtilityClass;
 
 import static io.github.cdimascio.dotenv.Dotenv.load;
 
+@UtilityClass
 public class EnvParser {
-    private final static Dotenv ENV = load();
+    private final Dotenv ENV = load();
 
-    public static String get(@NonNull final String key) {
+    public String get(@NonNull final String key) {
         return ENV.get(key);
     }
 }
